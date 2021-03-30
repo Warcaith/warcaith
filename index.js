778c75d84f4bf156b5722825a3ed15133df01936
Warcaith <kar18ga@student.ju.se>
2021-03-30 23:40:54 +0200

    feat: project setup
    added mustache and github workflow main.yaml configuration file

---
+++ index.js
@@ -0,0 +1,14 @@
+const Mustache = require("mustache");
+const fs = require("fs");
+
+const MUSTACHE_MAIN_LAYOUT = "./main.mustache";
+
+function generate() {
+    fs.readFile(MUSTACHE_MAIN_LAYOUT, (err, data) => {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), null);
+        fs.writeFileSync("README.md", output);
+    });
+}
+
+generate();
